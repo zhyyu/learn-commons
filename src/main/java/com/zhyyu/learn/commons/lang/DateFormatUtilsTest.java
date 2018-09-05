6896484ae57f765927b56f4d9305d5f9291955ad
zhongyu_yu <zhongyu_yu@kingdee.com>
2018-09-05 16:41:20 +0800

    DateFormatUtils.ISO_8601_EXTENDED_DATE_FORMAT.parse

--- src/main/java/com/zhyyu/learn/commons/lang/DateFormatUtilsTest.java
+++ src/main/java/com/zhyyu/learn/commons/lang/DateFormatUtilsTest.java
@@ -1,5 +1,6 @@
 package com.zhyyu.learn.commons.lang;
 
+import java.text.ParseException;
 import java.util.Date;
 
 import org.apache.commons.lang3.time.DateFormatUtils;
@@ -11,12 +12,15 @@ import org.apache.commons.lang3.time.DateFormatUtils;
  */
 public class DateFormatUtilsTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		String dateStr = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
 		System.out.println(dateStr);
 		
 		String dateStr8601 = DateFormatUtils.ISO_8601_EXTENDED_DATETIME_FORMAT.format(new Date());
 		System.out.println(dateStr8601);
+		
+		
+		System.out.println(DateFormatUtils.ISO_8601_EXTENDED_DATE_FORMAT.parse("2018-05-01"));
 	}
 	
 }
